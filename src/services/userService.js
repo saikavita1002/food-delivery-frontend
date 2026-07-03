7c23a5d776f34fccc8d1939bcc012dfb755dd4e3
saikavita1002 <saikavitainjiripu004@gmail.com>
2026-07-03 17:44:18 +0530

    Fixed API routes and updated frontend services

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import api from './api';
 
-export const registerUser = (data) => api.post('/users/register', data);
-export const loginUser = (data) => api.post('/users/login', data);
-export const getProfile = () => api.get('/users/profile');
-export const updateProfile = (data) => api.put('/users/profile', data);
-export const changePassword = (data) => api.put('/users/change-password', data);
+export const registerUser = (data) => api.post('api/users/register', data);
+export const loginUser = (data) => api.post('api/users/login', data);
+export const getProfile = () => api.get('api/users/profile');
+export const updateProfile = (data) => api.put('api/users/profile', data);
+export const changePassword = (data) => api.put('api/users/change-password', data);
